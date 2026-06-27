02f0ad8f1ba4855218e01ddb8303b6a7aeb9a8a5
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-27 11:20:03 +0200

    Placements set up

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TicTacToe{
 
     public static void main(String[] args) {
@@ -9,10 +11,21 @@ public class TicTacToe{
         {'-', '+', '-', '+', '-'},
         {' ', '|', ' ', '|', ' '}};
 
-        
+        // Displaying the board
         printGameBoard(gameBoard);
 
-        }
+        // Getting the position of an X or O from the user
+        Scanner posObj = new Scanner(System.in);
+        System.out.println("Enter your placement (1-9): ");
+        int pos = posObj.nextInt();   
+
+        System.out.println(pos);
+
+        placePiece(gameBoard, pos, null);
+
+        printGameBoard(gameBoard);
+
+    }
 
 
         // ============================================================
@@ -30,8 +43,46 @@ public class TicTacToe{
                 }
                 
                 System.out.println();
+            }
 
         }
+
+        // ============================================================
+        // X and O positioning
+        // ============================================================
+        public static void placePiece(char[][] gameBoard, int pos, String user){
+
+            switch(pos){
+                case 1:
+                    gameBoard[0][0] = 'X';
+                    break;
+                case 2:
+                    gameBoard[0][2] = 'X';
+                    break;
+                case 3:
+                    gameBoard[0][4] = 'X';
+                    break;
+                case 4:
+                    gameBoard[2][0] = 'X';
+                    break;
+                case 5:
+                    gameBoard[2][2] = 'X';
+                    break;
+                case 6:
+                    gameBoard[2][4] = 'X';
+                    break;
+                case 7:
+                    gameBoard[4][0] = 'X';
+                    break;
+                case 8:
+                    gameBoard[4][2] = 'X';
+                    break;
+                case 9:
+                    gameBoard[4][4] = 'X';
+                    break;
+            }
+
+        
 
     }
 
