8c2aedf7104a7ac1551c381e6269a6ecec647ba6
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-27 12:39:11 +0200

    Win condition set up

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe{
+
+    static ArrayList<Integer> playerPositions = new ArrayList<Integer>();
+    static ArrayList<Integer> cpuPositions = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         
@@ -32,6 +38,9 @@ public class TicTacToe{
             placePiece(gameBoard, cpuPos, "cpu");
 
             printGameBoard(gameBoard);
+
+            String result = checkWinner();
+            System.out.println(result);
         }
 
 
@@ -67,11 +76,13 @@ public class TicTacToe{
             if(user.equals("player")){
 
                 symbol = 'X';
+                playerPositions.add(pos);
 
             }
             else if(user.equals("cpu")){
 
                 symbol = 'O';
+                cpuPositions.add(pos);
 
             }
 
@@ -109,6 +120,57 @@ public class TicTacToe{
             }
 
         
+
+    }
+
+
+    // ============================================================
+    // Checking win conditions
+    // ============================================================
+    public static String checkWinner(){
+
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
+
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
+
+        List diag1 = Arrays.asList(1, 5, 9);
+        List diag2 = Arrays.asList(7, 5, 3);
+        
+        List<List> winning = new ArrayList<List>();
+        winning.add(topRow);
+        winning.add(midRow);
+        winning.add(botRow);
+        winning.add(leftCol);
+        winning.add(midCol);
+        winning.add(rightCol);
+        winning.add(diag1);
+        winning.add(diag2);
+
+        for(List l: winning){
+
+            if(playerPositions.containsAll(l)){
+
+                return "Congratulations you won!";
+
+            }
+            else if(cpuPositions.containsAll(l)){
+
+                return "CPU wins! Better luck next time.";
+
+            }
+            else if(playerPositions.size() + cpuPositions.size() == 9){
+
+
+
+            }
+
+        }
+
+        return "";
 
     }
 
