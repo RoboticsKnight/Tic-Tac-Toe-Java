85edae9f9d1b693c0bfd36180ab503755101a794
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-27 10:54:12 +0200

    Board set up

--- TicTacToe.java
+++ TicTacToe.java
@@ -2,7 +2,28 @@ public class TicTacToe{
 
     public static void main(String[] args) {
         
-        System.out.println("Hello world!!!");
+        // Board 2D array
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+        {'-', '+', '-', '+', '-'},
+        {' ', '|', ' ', '|', ' '},
+        {'-', '+', '-', '+', '-'},
+        {' ', '|', ' ', '|', ' '}};
+
+        // ============================================================
+        // Board display
+        // ============================================================
+        for(char[] r: gameBoard){
+
+            for(char c: r){
+
+                System.out.print(c);
+
+            }
+            
+            System.out.println();
+
+
+        }
 
     }
 
